923a967836e51f234ab48ca3ff09db9fe83e7340
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-10-17 22:04:33 +0200

    second commit
    step changed to 0.5

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ def calculate_wage(hours:float, rate:float)->float:
     return hours*rate
 
 name = st.text_input('Please enter your name: ')
-hours = st.number_input('Insert a number for hours ')
-rate = st.number_input('Insert a number for rate ')
+hours = st.number_input('Insert a number for hours ', step=0.5)
+rate = st.number_input('Insert a number for rate ', step=1)
 pay = calculate_wage(hours,rate)
 
 
